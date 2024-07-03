82604069b1747cf35ed193769fa2a848796eaee4
woojin <zaza0804@uos.ac.kr>
2024-07-04 04:00:40 +0900

    feat: add baekjoon problem Algorithm enums

--- src/main/kotlin/kote/demo/baekjoon/repository/AlgoTypeRepository.kt
+++ src/main/kotlin/kote/demo/baekjoon/repository/AlgoTypeRepository.kt
@@ -2,7 +2,8 @@ package kote.demo.baekjoon.repository
 
 import kote.demo.baekjoon.entity.AlgoType
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface AlgoTypeRepository: JpaRepository<AlgoType, Long>{
-    fun findByAlgoName(algoName:String):AlgoType
+    fun findByAlgoName(algoName:String): Optional<AlgoType>
 }
