72eb85a43e8878e0e1e69f331cf0f985c222b8d7
woojin <zaza0804@uos.ac.kr>
2024-07-10 00:41:31 +0900

    chore: remove unused code and html style

--- src/main/kotlin/kote/demo/baekjoon/dto/TestResponseDto.kt
+++
@@ -1,9 +0,0 @@
-package kote.demo.baekjoon.dto
-
-class TestResponseDto {
-    data class Problem(
-        var name: String?=null,
-        var level: String?=null,
-        var link: String?=null
-    )
-}
