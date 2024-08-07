3781e7ef2fdfc54cc30941addd82d2e7108ba6ea
woojin <zaza0804@uos.ac.kr>
2024-08-07 21:58:39 +0900

    feat: 유저 건의사항 추가

---
+++ src/main/kotlin/kote/demo/main/entity/Suggestion.kt
@@ -0,0 +1,15 @@
+package kote.demo.main.entity
+
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class Suggestion (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null,
+    val name:String,
+    val inquiry:String,
+    val message:String
+)
